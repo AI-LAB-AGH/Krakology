b570c1c55d85905dc11f19ab7166362da9045373
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2024-10-20 00:20:51 +0200

    Refactored and cleaned up the forecasting model

--- templates/dataloader.py
+++
@@ -1,8 +0,0 @@
-from torch.utils.data import DataLoader
-
-class MyDataloader(DataLoader):
-    def __init__(self) -> None:
-        pass
-
-    def __repr__(self) -> str:
-        return "Klasa do dataloadera"
