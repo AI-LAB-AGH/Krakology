6c85b4e23109c42efa0488a92cfa081dc02f5609
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2024-10-19 16:54:28 +0200

    Implemented basic torch Dataset

--- templates/dataset.py
+++ templates/dataset.py
@@ -1,11 +1,16 @@
 from torch.utils.data import Dataset
 
 class MyDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+    
+    def __len__(self):
+        return self.X.size[0]
 
     def __repr__(self) -> str:
         return "Class to load custom data from local source"
     
-    def __getitem__(self, index) -> None:
-        pass
+    def __getitem__(self, idx) -> None:
+        return self.X[idx], self.y[idx]
+
