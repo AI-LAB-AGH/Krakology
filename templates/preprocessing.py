d7cc69c947b92ef191401e438f1718ca33458ac1
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2024-10-19 15:22:53 +0200

    Implemented create_sequences in preprocessing.py

--- templates/preprocessing.py
+++ templates/preprocessing.py
@@ -1,2 +1,23 @@
 from sklearn.preprocessing import MinMaxScaler
+from typing import Tuple
 
+import pandas as pd
+import torch
+
+
+def create_sequences(data: pd.DataFrame, seq_length: int, target_name: str) -> Tuple[torch.Tensor, torch.Tensor]:
+    '''
+    Returns a pair: (feature_sequences, target_sequences) of torch tensors
+    '''
+
+    sequences = []
+    targets = []
+
+    for i in range(len(data) - seq_length):
+        seq = data.iloc[i:i + seq_length].drop(columns=[target_name]).values
+        sequences.append(seq)
+
+        tgt = data.iloc[i:i + seq_length][target_name]
+        targets.append(tgt)
+
+    return torch.tensor(sequences), torch.tensor(targets)
