9170b7eeac74c52ec16a58733da55f6f5b7fab8c
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2024-10-19 18:22:27 +0200

    Applied some fixes in create_sequences

--- templates/preprocessing.py
+++ templates/preprocessing.py
@@ -23,11 +23,13 @@ def create_sequences(data: pd.DataFrame, seq_length: int, target_name: str, pred
     sequences = []
     targets = []
 
-    for i in range(len(data) - seq_length):
+    data = data.drop(columns=['date'])
+
+    for i in range(len(data) - seq_length - prediction_horizon):
         seq = data.iloc[i:i + seq_length].drop(columns=[target_name]).values
         sequences.append(seq)
 
         tgt = data.iloc[i + seq_length:i + seq_length + prediction_horizon][target_name]
         targets.append(tgt)
 
-    return torch.tensor(sequences), torch.tensor(targets)
+    return torch.tensor(sequences), torch.tensor([t.to_numpy() for t in targets])
